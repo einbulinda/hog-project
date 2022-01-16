7b432ccdc8509b3c181eea7d14b2d2e12fb72d8a
einbulinda <einbulinda@gmail.com>
2022-01-16 19:05:09 +0300

    Allows cross origin requests

--- server.js
+++ server.js
@@ -26,26 +26,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 // Allow cross origin requests
-const whitelist = [
-  "http://localhost:3000",
-  "http://localhost:5000",
-  "https://hseofglamour.herokuapp.com/",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** origin of request" + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
-      callback(null, true);
-    } else {
-      console.log("Origin rejected");
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true }));
 
 // Routes
 app.use("/api/users", userRoutes);
